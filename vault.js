8ed0ea28212f984b8f23ee0480a3fff3ec05958d
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-05-30 14:50:13 -1000

    vault practice

--- vault.js
+++ vault.js
@@ -1,4 +1,25 @@
-'use strict';
+//'use strict';
 module.exports = function() {
-  
+
+return{
+
+
+getValue: function(key){
+if(!this.key){
+return null;
+}else{
+return this.key;
+}
+},
+
+setValue: function(key, value){
+this.key = value;
+
+
+}
+
+
+};
+
+
 };
